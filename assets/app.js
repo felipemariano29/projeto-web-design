37bc3e96fe3c8a66f841fa5a242fb7ed8a08f0ec
Felipe Mariano <felipemariano99@gmail.com>
2022-09-06 16:56:42 -0300

    Making navbar functional

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,61 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const
+    navbar = document.getElementById("navbar"),
+    navItens = document.querySelectorAll(".nav-item"),
+    setaBaixo = document.getElementById("seta-baixo");
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+let
+    altura = 983;
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+setaBaixo.addEventListener("click", () => {document.documentElement.scrollTop = 1*altura})
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= 980) {
+window.onscroll = () => { sempreNoTopo() }
+
+navItens.forEach(e => {
+    e.addEventListener('click', () => {
+        let altura = 983;
+        if(e.innerText==="home") document.documentElement.scrollTop = 0*altura;
+        else if(e.innerText==="info") document.documentElement.scrollTop = 1*altura;
+        else if(e.innerText==="geral") document.documentElement.scrollTop = 2*altura;
+        else if(e.innerText==="ods") document.documentElement.scrollTop = 3*altura;
+        else if(e.innerText==="gráficos") document.documentElement.scrollTop = 4*altura;
+        else if(e.innerText==="ranking") document.documentElement.scrollTop = 5*altura;
+    })
+})
+
+function removeCorNav(){
+    for(let i=0;i<navItens.length;i++) {
+        navItens[i].classList.remove("nav-item-active");
+    }
+}
+
+function verificaAltura() {
+    let altura = 950;
+    if(altura < window.scrollY && window.scrollY < (altura*2)){
+        removeCorNav();
+        navItens[1].classList.add("nav-item-active");
+    }
+    if((altura*2) < window.scrollY && window.scrollY < (altura*3)){
+        removeCorNav();
+        navItens[2].classList.add("nav-item-active");
+    }
+    if((altura*3) < window.scrollY && window.scrollY < (altura*4)){
+        removeCorNav();
+        navItens[3].classList.add("nav-item-active");
+    }
+    if((altura*4) < window.scrollY && window.scrollY < (altura*5)){
+        removeCorNav();
+        navItens[4].classList.add("nav-item-active");
+    }
+    if((altura*5) < window.scrollY && window.scrollY < (altura*6)){
+        removeCorNav();
+        navItens[5].classList.add("nav-item-active");
+    }
+}
+
+setInterval(verificaAltura, 100);
+
+function sempreNoTopo() {
+    if (window.pageYOffset >= altura) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
