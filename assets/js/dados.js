10f33cbff87fef3d7c845a2a4c7fffee08819824
Felipe Mariano <felipemariano99@gmail.com>
2022-09-07 18:51:42 -0300

    Bug Fix

--- assets/js/dados.js
+++ assets/js/dados.js
@@ -4,119 +4,119 @@ const
             'id': 1,
             'titulo': 'Erradicação da Pobreza',
             'descricao': 'Acabar com a pobreza em todas as suas formas, em todos os lugares',
-            'icone': '/assets/ods-icones/erradicacao-da-pobreza.svg',
+            'icone': '../assets/ods-icones/erradicacao-da-pobreza.svg',
             'pontos': '52,08 pontos'
         },
         {
             'id': 2,
             'titulo': 'Fome Zero e Agricultura Sustentável',
             'descricao': 'Acabar com a fome, alcançar a segurança alimentar e melhoria da nutrição e promover a agricultura sustentável',
-            'icone': '/assets/ods-icones/fome-zero.svg',
+            'icone': '../assets/ods-icones/fome-zero.svg',
             'pontos': '42,47 pontos'
         },
         {
             'id': 3,
             'titulo': 'Saúde e Bem Estar',
             'descricao': 'Assegurar uma vida saudável e promover o bem-estar para todos, em todas as idades',
-            'icone': '/assets/ods-icones/saude-e-bem-estar.svg',
+            'icone': '../assets/ods-icones/saude-e-bem-estar.svg',
             'pontos': '66,4 Pontos'
         },
         {
             'id': 4,
             'titulo': 'Educação de Qualidade',
             'descricao': 'Assegurar a educação inclusiva e equitativa de qualidade, e promover oportunidades de aprendizagem ao longo da vida para todos',
-            'icone': '/assets/ods-icones/educacao-de-qualidade.svg',
+            'icone': '../assets/ods-icones/educacao-de-qualidade.svg',
             'pontos': '44,61 pontos'
         },
         {
             'id': 5,
             'titulo': 'Igualdade de Gênero',
             'descricao': 'Alcançar a igualdade de gênero e empoderar todas as mulheres e meninas',
-            'icone': '/assets/ods-icones/igualdade-de-genero.svg',
+            'icone': '../assets/ods-icones/igualdade-de-genero.svg',
             'pontos': '28,92 pontos'
         },
         {
             'id': 6,
             'titulo': 'Água Potável e Saneamento',
             'descricao': 'Assegurar a disponibilidade e a gestão sustentável da água e saneamento para todos',
-            'icone': '/assets/ods-icones/agua-potavel.svg',
+            'icone': '../assets/ods-icones/agua-potavel.svg',
             'pontos': '54,89 pontos'
         },
         {
             'id': 7,
             'titulo': 'Energia Acessível e Limpa',
             'descricao': 'Assegurar o acesso confiável, sustentável, moderno e a preço acessível à energia para todos',
-            'icone': '/assets/ods-icones/energia-limpa.svg',
+            'icone': '../assets/ods-icones/energia-limpa.svg',
             'pontos': '59,38 pontos'
         },
         {
             'id': 8,
             'titulo': 'Trabalho Decente e Crescimento Econômico',
             'descricao': 'Promover o crescimento econômico sustentado, inclusivo e sustentável, o emprego pleno e produtivo e o trabalho decente para todos',
-            'icone': '/assets/ods-icones/trabalho-economico.svg',
+            'icone': '../assets/ods-icones/trabalho-economico.svg',
             'pontos': '47,26 pontos'
         },
         {
             'id': 9,
             'titulo': 'Indústria Inovação e Infraestrutura',
             'descricao': 'Construir infraestruturas resilientes, promover a industrialização inclusiva e sustentável e fomentar a inovação',
-            'icone': '/assets/ods-icones/industria-inovacao.svg',
+            'icone': '../assets/ods-icones/industria-inovacao.svg',
             'pontos': '56,39 pontos'
         },
         {
             'id': 10,
             'titulo': 'Redução das Desigualdades',
             'descricao': 'Reduzir a desigualdade dentro dos países e entre eles',
-            'icone': '/assets/ods-icones/reducao-das-desigualdades.svg',
+            'icone': '../assets/ods-icones/reducao-das-desigualdades.svg',
             'pontos': '58,29 pontos'
         },
         {
             'id': 11,
             'titulo': 'Cidades e Comunidades Sustentáveis',
             'descricao': 'Tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis',
-            'icone': '/assets/ods-icones/cidades-e-comunidades.svg',
+            'icone': '../assets/ods-icones/cidades-e-comunidades.svg',
             'pontos': '72,27 pontos'
         },
         {
             'id': 12,
             'titulo': 'Consumo e Produção Responsáveis',
             'descricao': 'Assegurar padrões de produção e de consumo sustentáveis',
-            'icone': '/assets/ods-icones/consumo-responsavel.svg',
+            'icone': '../assets/ods-icones/consumo-responsavel.svg',
             'pontos': '39,79 pontos'
         },
         {
             'id': 13,
             'titulo': 'Ação Contra a Mudança Global do Clima',
             'descricao': 'Tomar medidas urgentes para combater a mudança do clima e seus impactos',
-            'icone': '/assets/ods-icones/mudanca-global.svg',
+            'icone': '../assets/ods-icones/mudanca-global.svg',
             'pontos': '60,66 pontos'
         },
         {
             'id': 14,
             'titulo': 'Vida na Água',
             'descricao': 'Conservar e promover o uso sustentável dos oceanos, dos mares e dos recursos marinhos para o desenvolvimento sustentável',
-            'icone': '/assets/ods-icones/vida-na-agua.svg',
+            'icone': '../assets/ods-icones/vida-na-agua.svg',
             'pontos': '19,08 pontos'
         },
         {
             'id': 15,
             'titulo': 'Vida Terrestre',
             'descricao': 'Proteger, recuperar e promover o uso sustentável dos ecossistemas terrestres, gerir de forma sustentável as florestas, combater a desertificação, deter e reverter a degradação da terra e deter a perda',
-            'icone': '/assets/ods-icones/vida-terrestre.svg',
+            'icone': '../assets/ods-icones/vida-terrestre.svg',
             'pontos': '23,35 pontos'
         },
         {
             'id': 16,
             'titulo': 'Paz, Justiça e Instituições Eficazes',
             'descricao': 'Promover sociedades pacíficas e inclusivas para o desenvolvimento sustentável, proporcionar o acesso à justiça para todos e construir instituições eficazes, responsáveis e inclusivas em todos os níveis',
-            'icone': '/assets/ods-icones/paz-justica.svg',
+            'icone': '../assets/ods-icones/paz-justica.svg',
             'pontos': '57,90 pontos'
         },
         {
             'id': 17,
             'titulo': 'Parcerias e Meios de Implementação',
             'descricao': 'Fortalecer os meios de implementação e revitalizar a parceria global para o desenvolvimento sustentável',
-            'icone': '/assets/ods-icones/parcerias.svg',
+            'icone': '../assets/ods-icones/parcerias.svg',
             'pontos': '13,92 pontos'
         }
     ],
